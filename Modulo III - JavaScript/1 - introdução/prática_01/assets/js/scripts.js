9a4456ec2bd582b173b66c07a588c08a46094450
Matheus Barcellos <matbarcellos21@gmail.com>
2022-05-06 02:35:12 +0000

    Create scripts.js

---
+++ Modulo III - JavaScript/1 - introdução/prática_01/assets/js/scripts.js
@@ -0,0 +1,13 @@
+var currentNumberWrapper = document.getElementById("currentNumber")
+var currentNumber = 0
+
+
+function increment() {
+    currentNumber += 1
+    currentNumberWrapper.innerHTML = currentNumber
+}
+
+function decrement() {
+    currentNumber -= 1
+    currentNumberWrapper.innerHTML = currentNumber
+}
