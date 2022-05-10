a4fc969c0507ead1727317bceb2587ded56a4d2e
Matheus Barcellos <matbarcellos21@gmail.com>
2022-05-10 00:06:42 +0000

    included-alert

--- Modulo III - JavaScript/1 - introdução/prática_01/assets/scripts.js
+++ Modulo III - JavaScript/1 - introdução/prática_01/assets/scripts.js
@@ -1,13 +1,21 @@
 var currentNumberWrapper = document.getElementById("currentNumber")
 var currentNumber = 0
 
-
 function increment() {
-    currentNumber += 1
+    if (currentNumber >= 10) {
+        alert("nao pode numero maior que 10")
+    } else{
+    currentNumber +=1
     currentNumberWrapper.innerHTML = currentNumber
+    }
 }
 
 function decrement() {
+    
+    if (currentNumber <= 0) {
+        alert("nao pode numero negativo")
+    } else{
     currentNumber -= 1
     currentNumberWrapper.innerHTML = currentNumber
+    }
 }
