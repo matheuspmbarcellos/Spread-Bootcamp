3a0f1d14a571ef5e4dc85d402f193641e28c0d2f
Matheus Barcellos <matbarcellos21@gmail.com>
2022-06-03 23:37:51 -0300

    Create desafio.js

---
+++ Modulo III - JavaScript/13 - desafios iniciais js spread/desafio.js
@@ -0,0 +1,22 @@
+/* let lines = gets().split("\n");
+
+let N = parseInt(lines.shift());
+for (                         ) {
+    if (           ) frase +=      ;
+    else frase +=        ;
+}
+
+print(frase); */
+
+
+let N = 5
+const frase = []
+
+for(let i = 0; i < N - 1; i++) {
+    frase.push("Ho")
+}
+frase.push("Ho!")
+
+
+
+console.log(frase.join(' '))
